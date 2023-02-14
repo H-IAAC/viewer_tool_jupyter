96c52faca2cea4eb7bacf28cac1f2d05e194ed7b
Saulo A. Moraes <saulo.moraes@eldorado.org.br>
2023-02-14 16:34:37 -0300

    update viewer_tool lib to print download status and support csv and video download

--- librepo/viewer_tool.py
+++ librepo/viewer_tool.py
@@ -3,13 +3,19 @@ import urllib.parse
 import pandas as pd
 from io import StringIO
 import json
+import csv
 import os
-from IPython.display import display, HTML, IFrame
+from IPython.display import display, HTML
+import shutil
+from tqdm.auto import tqdm
 
 class ViewerTool:    
     _url = "http://vm.hiaac.ic.unicamp.br:8081/"
     #_url = "http://localhost:8080/"
     _api = "api/"
+    
+    def setup(self, url, port):
+        self._url = url + ':' + port + '/'
     
     # 
     #    
@@ -30,14 +36,41 @@ class ViewerTool:
     # @return 
     def list_experiment_files(self, experiment, activity, user):
         query = {'experiment':experiment, 'activity':activity, 'user':user}
-        data = self._request("list_experiment_files", query).json()       
+        data = self._request("list_experiment_files", query).json()
+        
+        if not data:
+            print("No data found!")
+            return None
+        
         df = pd.DataFrame(data['files'], columns=["Files"])
         return df
     
-    def get_csv(self, experiment, activity, user, filename):
+    def get_csv(self, experiment, activity, user, filename, download=False):
         csv_url = self._format_as_download_url(experiment, activity, user, filename)
-        df = pd.read_csv(csv_url, sep=',', skipinitialspace=True)
+        
+        # download file
+        if download:
+            self.download_file(experiment, activity, user, filename, './Data/' + experiment + os.sep + user + os.sep + activity)
+        
+        try:
+            df = pd.read_csv(csv_url, sep=';', skipinitialspace=True)
+            df.Timestamp
+        except AttributeError:
+            df = pd.read_csv(csv_url, sep=',', skipinitialspace=True)
+        except urllib.error.HTTPError as errh:
+            print(f'Failed to download {filename}: {errh}')
+            return None
+            
         return df
+
+    def get_video(self, experiment, activity, user, filename, download=False):
+        video_url = self._format_as_download_url(experiment, activity, user, filename)
+
+        # download file
+        if download:
+            self.download_file(experiment, activity, user, filename, './Data/' + experiment + os.sep + user + os.sep + activity)
+
+        return video_url
 
     def display_video(self, experiment, activity, user, filename):
         video_url = self._format_as_download_url(experiment, activity, user, filename)
@@ -46,16 +79,32 @@ class ViewerTool:
                          </video>
                      """)
         return video
-    def get_url_video(self, experiment, activity, user, filename):
-        video_url = self._format_as_download_url(experiment, activity, user, filename)        
-        return video_url
 
     def download_file(self, experiment, activity, user, filename, directory_location):
         file_url = self._format_as_download_url(experiment, activity, user, filename)
-        result = requests.get(file_url, timeout=5)
-        directory_location += os.sep + filename
-        open(directory_location, 'wb').write(result.content)
-        print(f'Saving {filename} as {directory_location}')
+        
+        # make an HTTP request within a context manager
+        try:
+            with requests.get(file_url, stream=True) as r:
+                
+                r.raise_for_status()
+                
+                # check header to get content length, in bytes
+                total_length = int(r.headers.get("Content-Length"))
+
+                file_location = directory_location + os.sep + filename  
+                print(f'Saving {filename} to {file_location}')
+                
+                # implement progress bar via tqdm
+                with tqdm.wrapattr(r.raw, "read", total=total_length, desc="")as raw:
+                    # Create directory_location
+                    os.makedirs(directory_location, exist_ok=True)
+
+                    with open(f"{file_location}", 'wb') as output:
+                        shutil.copyfileobj(raw, output)
+                        
+        except requests.exceptions.HTTPError as errh:
+            print('File not found!')
     
     def _format_as_download_url(self, experiment, activity, user, filename):
          params = urllib.parse.quote(experiment + ' [' + activity + '] [' + user + ']/' + filename)
