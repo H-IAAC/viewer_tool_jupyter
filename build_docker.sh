2737212733b0fd0446cead394f58e0a1e6c9d6c1
amparo munoz <amparomunoz>
2023-08-15 09:40:54 -0300

    removing files

--- build_docker.sh
+++
@@ -1,1 +0,0 @@
-docker build -t hiaac4-visualization .
--- run_jupyter.sh
+++
@@ -1,2 +0,0 @@
-export PORT=8888
-docker run  -it --rm  -e SHELL="/bin/bash" -e HOME=`pwd` -p ${PORT}:${PORT} -v`pwd`:`pwd` -w`pwd` -u $(id -u ${USER}):$(id -g ${USER}) hiaac4-visualization python -m jupyterlab --port ${PORT} --no-browser --ip='0.0.0.0' --NotebookApp.token='' --NotebookApp.password=''
